1945b2d53395eb57093404fb64f89a7801a034d2
git-thinh <33323583+git-thinh@users.noreply.github.com>
2018-01-08 16:18:42 +0700

    a

--- main.cpp
+++ main.cpp
@@ -7,16 +7,11 @@ int main()
 	_testClass* p1 = new _testClass;
 	_testClass* p2 = new _testClass; 
 
-	___msgPublish::getInstance().sendBroadcast((char *)"123");
-	//___msgPublish::getInstance().sendBroadcast("2");
-	//___msgPublish::getInstance().sendBroadcast("3");
-	//___msgPublish::getInstance().sendBroadcast("4");
-	//___msgPublish::getInstance().sendBroadcast("5");
-
+	std::cout << "Input text to send broadCast: ";
 	std::string a;
 	while (a != "exit") {
 		std::cin >> a;
-		___msgPublish::getInstance().sendBroadcast((char*)a.c_str());
+		___msgPublish::getInstance().sendBroadcast((char*)a.c_str());		
 	} 
 
 	___msgPublish::getInstance().release();
